b5b89ba7990f018da621e8ff5191b89590679548
matheushw <cunhamatheusc@gmail.com>
2020-10-17 03:41:02 -0300

    Almost done

--- TP1.asm
+++ TP1.asm
@@ -8,35 +8,77 @@
 org 100h
 
 Inicio:
-    MOV CL, 10
-    JMP RotinaPrincipal
-
-RotinaPrincipal:
-    MOV BX, Target
+    MOV CL, 5
+    JMP CheckBotao
+    
+CheckBotao:
+    MOV BX, Botao_parar_elevador
+    MOV AL, [BX]
+    CMP AL, 1
+    JZ ElevadorParado
+    
+ChekcDefTarget:    
+    MOV BX, ReqQuant
     MOV DL, [BX]
     CMP DL, 0
-    JNZ GoToTarget
+    JZ CheckTeclado
     
-    MOV BX, Recebeu_comando
-    MOV DL, [BX]
-    CMP DL, 0
-    JNZ DefinirTarget
+    MOV BX, EmMovimento
+    MOV DH, [BX]
+    CMP DH, 0
+    JZ DefinirTarget
     
+CheckTeclado:    
     DEC CL
-    JNZ RotinaPrincipal
+    JNZ CheckBotao
     MOV AH, 1
     INT 16h
     JNZ InputTeclado
     
-    
+ChecGoTarget:    
+    MOV BX, Target
+    MOV DL, [BX]
+    CMP DL, 0
+    JNZ GoToTarget 
     JMP Inicio
-    
 
 InputTeclado:
     MOV AH, 0
     INT 16h
+
+CheckPararExecucao:    
     CMP AL, 1bh
     JZ PararExecucao
+                   
+CheckPararElevador:
+    CMP AL, 112
+    JZ PararElevador
+                   
+CheckLivrarElevador:
+    CMP AL, 80
+    JZ LivrarElevador
+    
+CheckObsElevador:
+    CMP AL, 115
+    JZ CheckPorta
+                   
+CheckDesobsElevador:
+    CMP AL, 83
+    JZ DesobistruirElevador  
+                   
+CheckElevadorParado:
+    MOV BX, Botao_parar_elevador 
+    MOV DL, [BX]
+    CMP DL, 1
+    JZ Inicio         
+                    
+IncReqQuant:
+    MOV BX, ReqQuant
+    MOV CL, [BX]
+    INC CL
+    MOV ReqQuant, CL
+            
+CheckComando:
     MOV BX, Sensor_passou_andar
     MOV CL, [BX]
     CMP AL, 61h
@@ -49,7 +91,7 @@ ComandoInterno:
     MOV BL, AL
     MOV ReqI[BX], 1
     MOV Recebeu_comando, 1
-    JMP Inicio
+    JMP CheckObstruido
 
 ComandoExterno:
     SUB AL, 31h
@@ -57,6 +99,13 @@ ComandoExterno:
     MOV BL, AL
     MOV ReqE[BX], 1
     MOV Recebeu_comando, 1
+    JMP CheckObstruido
+    
+CheckObstruido:
+    MOV BX, Sensos_preseca
+    MOV DL, [BX]
+    CMP DL, 1
+    JZ Obstruido
     JMP Inicio
 
 DefinirTarget:
@@ -64,11 +113,11 @@ DefinirTarget:
     MOV BX, 7
     CALL DefinirTargetLoopE
     MOV BX, Target
-    MOV AX, [BX]
-    CMP AX, 0
-    JZ DefinirTargetLoopI
+    MOV AL, [BX]
     MOV BX, 7
-    JMP RotinaPrincipal
+    CMP AL, 0
+    JZ DefinirTargetLoopI
+    JMP CheckBotao
 
 DefinirTargetLoopE:
     MOV AL, ReqE[BX] 
@@ -89,19 +138,23 @@ DefinirTargetLoopI:
 FocusTarget:
     INC BX
     MOV Target, BL
-    JMP RotinaPrincipal
+    MOV EmMovimento, 1
+    JMP CheckBotao
 
-GoToTarget:
+GoToTarget:  
     MOV BX, Target
     MOV AL, [BX]
     MOV BX, Sensor_passou_andar
     MOV AH, [BX]
     CMP AH, AL
     JZ ChegouTarget
-    JS SubirAndar
-    JNS DescerAndar
+    JMP FecharPorta
 
 ChegouTarget:
+    MOV BX, ReqQuant
+    MOV AL, [BX]
+    DEC AL
+    MOV ReqQuant, AL
     MOV BX, Target
     MOV AL, [BX]
     DEC AL
@@ -110,6 +163,11 @@ ChegouTarget:
     MOV ReqE[BX], 0
     MOV ReqI[BX], 0
     MOV Target, 0
+    MOV EmMovimento, 0
+    MOV DX, ABREPORTA
+    CALL PrintMsg
+    MOV Porta_Aberta, 1
+    CALL PrintStatus 
     JMP Inicio
 
 SubirAndar:
@@ -133,7 +191,38 @@ DescerAndar:
     MOV  DX, DESCEU
     CALL PrintMsg
     JMP Inicio
+    
+FecharPorta:
+    MOV BX,Porta_Aberta
+    MOV DL, [BX]
+    CMP DL, 1
+    JZ PrintFecharPorta
+    CALL PrintStatus
+    JMP Movimentar
+    
+Movimentar:
+    MOV BX, Target
+    MOV AL, [BX]
+    MOV BX, Sensor_passou_andar
+    MOV AH, [BX]
+    CMP AH, AL
+    JS SubirAndar
+    JNS DescerAndar
 
+Obstruido:
+    MOV DX,PORTAOBSTRUIDA
+    CALL PrintMsg
+    MOV AH, 1
+    INT 16h
+    JNZ InputTeclado
+    JMP Obstruido    
+
+PrintFecharPorta:
+    MOV Porta_Aberta, 0   
+    MOV DX, FECHAPORTA
+    CALL PrintMsg
+    JMP FecharPorta
+    
 ElevadorNoTopo:
     MOV Elevador_no_Topo, 1
     MOV  DX, SUBIU
@@ -145,28 +234,202 @@ ElevadorNaBase:
     MOV  DX, DESCEU
     CALL PrintMsg
     JMP Inicio
+    
+ElevadorParado: ;Desvio executado caso alguem tenha apertado o botao de parar o elevador
+    MOV DX,PARADO
+    CALL PrintMsg
+    MOV AH, 1
+    INT 16h
+    JNZ InputTeclado
+    JMP ElevadorParado
+
+ObstruirElevador:
+    MOV Sensos_preseca, 1
+    JMP Obstruido
+    
+DesobistruirElevador:
+    MOV Sensos_preseca, 0
+    JMP Inicio
+
+PararElevador: ;Desvio executado quando alguem aperta o botao de parar o elevador
+    MOV Botao_parar_elevador, 1
+    JMP Inicio
+    
+LivrarElevador: ;Desvio executado quando alguem aperta o botao de Desbloquear o elevador
+    MOV Botao_parar_elevador, 0
+    JMP Inicio
 
 PrintMsg:   ;Usando a interrupcao para printar uma cadeia de caracteres
-    MOV  AH, 09h 
-    INT  21h
+    MOV AH, 09h 
+    INT 21h
     RET
+    
+PrintNwl:   ;Printa quebra de linha
+    MOV DX, QUEBRADELINHA 
+    MOV AH, 09h 
+    INT 21h
+    RET
+    
+PrintStatus:
+    CALL PrintAndarStatus
+    CALL PrintNwl
+                
+    CALL PrintStatusCabine
+    CALL PrintNwl
+                
+    MOV BX, 0
+    CALL PrintReqI
+    CALL PrintNwl
+    
+    MOV BX, 0
+    CALL PrintReqE
+    CALL PrintNwl
+    
+    RET          
+    
+PrintStatusCabine:
+    MOV DX, SITUACAO
+    CALL PrintMsg
+    MOV BX, Porta_Aberta
+    MOV DL, [BX]
+    CMP DL, 1 
+    JZ PrintPortaAberta
+    JMP PrintPortaFechada
+           
+PrintPortaAberta:
+    MOV DX, PORTAABERTA
+    CALL PrintMsg
+    MOV BX, EmMovimento
+    MOV DL, [BX]
+    CMP DL, 1
+    JZ PrintStatusMovimento
+    JMP PrintParado 
+    
+PrintPortaFechada:
+    MOV DX, PORTAFECHADA
+    CALL PrintMsg
+    MOV BX, EmMovimento
+    MOV DL, [BX]
+    CMP DL, 1
+    JZ PrintStatusMovimento
+    JMP PrintParado
+
+PrintParado:
+    MOV DX, ELEVPARADO
+    CALL PrintMsg
+    RET
+    
+PrintStatusMovimento:
+    MOV DX, MOVENDOPARA
+    CALL PrintMsg
+    MOV BX, Target
+    MOV BX, [BX]
+    DEC BX
+    CALL PrintNumeroAndar
+    RET 
+
+PrintReqI:
+    MOV DX, LISTREQI
+    CALL PrintMsg
+    JMP LoopPrintReqI  
+
+LoopPrintReqI:   
+    MOV DL, ReqI[BX]
+    CMP DL, 1
+    JZ PrintRequestAndarI
+    INC BX
+    CMP BX, 8
+    JNZ LoopPrintReqI
+    RET
+
+PrintRequestAndarI:
+    CALL PrintNumeroAndar
+    INC BX
+    CMP BX, 8
+    JNZ LoopPrintReqI
+    RET
+    
+PrintReqE:
+    MOV DX, LISTREQE
+    CALL PrintMsg
+    JMP LoopPrintReqE
+    
+LoopPrintReqE:   
+    MOV DL, ReqE[BX]
+    CMP DL, 1
+    JZ PrintRequestAndarE
+    INC BX
+    CMP BX, 8
+    JNZ LoopPrintReqE
+    RET
+
+PrintRequestAndarE:
+    CALL PrintNumeroAndar
+    INC BX
+    CMP BX, 8
+    JNZ LoopPrintReqE
+    RET       
+    
+PrintNumeroAndar:
+    MOV AL, BL
+    INC AL
+    ADD AL, 30h
+    MOV AH, 0eh
+    INT 10h
+    MOV DX, BLANKSPACE
+    CALL PrintMsg
+    RET
+    
+PrintAndarStatus:
+    MOV DX, PASSOUANDAR
+    CALL PrintMsg
+    MOV BX, Sensor_passou_andar
+    MOV AL, [BX]
+    ADD AL, 30h
+    MOV AH, 0eh
+    INT 10h
+    RET
+
+CheckPorta:
+    MOV BX, Porta_Aberta
+    MOV DL, [BX]
+    CMP DL, 1
+    JZ ObstruirElevador
+    JMP CheckDesobsElevador
 
 PararExecucao:
     NOP
     RET
 
-ReqE: DB 0, 0, 0, 0, 0, 0, 0, 0 ;Implementar
-ReqI: DB 0, 0, 0, 0, 0, 0, 0, 0 ;Implementar
-Contadora: DB 0 ;Implementar
+ReqE: DB 0, 0, 0, 0, 0, 0, 0, 0
+ReqI: DB 0, 0, 0, 0, 0, 0, 0, 0
+Botao_parar_elevador: DB 0
 Elevador_na_Base: DB 0
 Elevador_no_Topo: DB 0
-Porta_Aberta: DB 0 ;Implementar
+Porta_Aberta: DB 1
 Sensos_preseca: DB 0 ;Implementar
 Sensor_passou_andar: DB 1
 Recebeu_comando: DB 0
+EmMovimento: DB 0
 Target: DB 0
+ReqQuant: DB 0
+FlagRegQuant: DB 0
 
-SUBIU: DB  "Move para cima $" 
-DESCEU: DB  "Move para baixo $"
-ABREPORTA: DB  "Abre Porta $"
-FECHAPORTA: DB  "Fecha Porta $"
+SUBIU: DB "Move para cima", 0Dh,0Ah, "$" 
+DESCEU: DB "Move para baixo", 0Dh,0Ah, "$"
+ABREPORTA: DB "Abre Porta", 0Dh,0Ah, "$"
+FECHAPORTA: DB "Fecha Porta", 0Dh,0Ah, "$"
+PARADO: DB "Elevador parado", 0Dh,0Ah, "$"
+PORTAOBSTRUIDA: DB "Porta Obstruida", 0Dh,0Ah, "$"
+
+PORTAABERTA: DB "Porta Aberta, $"
+PORTAFECHADA: DB "Porta Fechada, $"
+ELEVPARADO: DB "Parado $"
+MOVENDOPARA: DB "Movendo para o andar $"
+                                              
+QUEBRADELINHA: DB 0Dh,0Ah,"$"
+PASSOUANDAR: DB "Andar Atual -> $"
+SITUACAO: DB "Situacao da cabine -> $"
+LISTREQE: DB "Lista de requisicoes externas -> $"
+LISTREQI: DB "Lista de requisicoes internas -> $"
+BLANKSPACE: DB " $"
